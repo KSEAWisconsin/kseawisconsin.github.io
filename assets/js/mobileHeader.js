962e93c442b8b30d550d681e2f9cbcf629b084ce
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2019-11-17 01:09:47 -0600

    Move working file to header.html / need to modify design

---
+++ assets/js/mobileHeader.js
@@ -0,0 +1,12 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function toggleMenu() {
+    var navLinks = document.getElementById("navLinks");
+    var menuButton = document.getElementById("menuButton");
+    if (navLinks.style.display === "block") {
+        navLinks.style.display = "none";
+        menuButton.style.backgroundColor = "#B2E4F6";
+    } else {
+        navLinks.style.display = "block";
+        menuButton.style.backgroundColor = "#01C1F1";
+    }
+}
