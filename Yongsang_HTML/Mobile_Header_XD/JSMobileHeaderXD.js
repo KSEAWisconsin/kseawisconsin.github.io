5c6cee98434cc0ee9aad000f6c12ca4237daf7f4
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2019-11-17 00:27:35 -0600

    Menu Background color changing

--- Yongsang_HTML/Mobile_Header_XD/JSMobileHeaderXD.js
+++ Yongsang_HTML/Mobile_Header_XD/JSMobileHeaderXD.js
@@ -1,9 +1,12 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function toggleMenu() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
+    var myLinks = document.getElementById("myLinks");
+    var menu = document.getElementById("menu");
+    if (myLinks.style.display === "block") {
+        myLinks.style.display = "none";
+        menu.style.backgroundColor = "#B2E4F6";
     } else {
-        x.style.display = "block";
+        myLinks.style.display = "block";
+        menu.style.backgroundColor = "#01C1F1";
     }
 }
