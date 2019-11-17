a5b672970a8185ce4219d103674e8020eec40549
Hyecheol (Jerry) Jang <hyecheol123@gmail.com>
2019-11-16 23:47:40 -0600

    Used CSS Grid / Need to fix design

--- Yongsang_HTML/Mobile_Header_XD/JSMobileHeaderXD.js
+++ Yongsang_HTML/Mobile_Header_XD/JSMobileHeaderXD.js
@@ -1,5 +1,5 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
+function toggleMenu() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
