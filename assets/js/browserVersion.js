a6f3afb702fd02270f4a83840cbcfc50aa4ccd42
Hyecheol (Jerry) Jang <hyecheol.jang@wisc.edu>
2021-01-04 14:30:57 +0900

    browserVersion.js: check whether browser support grid layout or not

--- assets/js/browserVersion.js
+++ assets/js/browserVersion.js
@@ -1,45 +1,18 @@
-function get_browser() {
-  var ua=navigator.userAgent,tem,M=ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || []; 
-  if(/trident/i.test(M[1])){
-      tem=/\brv[ :]+(\d+)/g.exec(ua) || []; 
-      return {name:'IE',version:(tem[1]||'')};
-      }   
-  if(M[1]==='Chrome'){
-      tem=ua.match(/\bOPR|Edge\/(\d+)/)
-      if(tem[0].indexOf('Edge') != -1) { return{name:'Edge', version:tem[1]};}
-      if(tem!=null)   {return {name:'Opera', version:tem[1]};}
-    }   
-  M=M[2]? [M[1], M[2]]: [navigator.appName, navigator.appVersion, '-?'];
-  if((tem=ua.match(/version\/(\d+)/i))!=null) {M.splice(1,1,tem[1]);}
-  return {
-    name: M[0],
-    version: M[1]
-  };
-}
+/**
+ * Check whether the client browser supports grid or not
+ * @author Hyecheol (Jerry) Jang
+ */
 
-var browser = get_browser();
-if(browser.name.match('IE')) {
-  alert("Internet Explorer Not Supported. Use the Edge of the other browser");
-} else if(browser.name.match('Edge')) {
-  if(parseFloat(browser.version) < 16) {
-    alert("This Edge version is not supported. Please update your browser");
+ // Check only if the browser is not set previously
+if(sessionStorage.getItem('supported') == undefined) {
+  const testElem = document.createElement('div'); // dummy div class to check the browser supports gird or not
+  if (testElem.style.grid !== undefined) { // Supported
+    sessionStorage.setItem('supported', true);
+  } else { // Not supported
+    // Prompt Message
+    if(confirm('This browser is not supported.\nUse Latest Chrome/Firefox/Edge/Safari for better experience.')) {
+      // If user confirms the message, does not check for the support anymore for current session
+      sessionStorage.setItem('supported', false);
+    }
   }
-} else if(browser.name.match('Firefox')) {
-  if(parseFloat(browser.version) < 59) {
-    alert("This Firefox version is not supported. Please update your browser");
-  }
-} else if(browser.name.match('Chrome')) {
-  if(parseFloat(browser.version) < 66) {
-    alert("This Chrome version is not supported. Please update your browser");
-  }
-} else if(browser.name.match('Safari')) {
-  if(parseFloat(browser.version) < 10.1) {
-    alert("This Safari version is not supported. Please update your browser");
-  }
-} else if(browser.name.match('Opera')) {
-  if(parseFloat(browser.version) < 53) {
-    alert("This Opera version is not supported. Please update your browser");
-  }
-} else {
-  alert("Cannot detect browser version.");
 }
